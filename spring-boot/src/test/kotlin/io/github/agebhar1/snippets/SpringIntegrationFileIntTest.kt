ce158cff13d4d9f701f4c84aaf29dd23f1a402c8
Andreas Gebhardt <agebhar1@googlemail.com>
2021-05-24 18:02:16 +0200

    add snippet for »Spring Boot« w/ »Spring Integration« file inbound adapter using 'WatchService'

---
+++ spring-boot/src/test/kotlin/io/github/agebhar1/snippets/SpringIntegrationFileIntTest.kt
@@ -0,0 +1,82 @@
+package io.github.agebhar1.snippets
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.io.TempDir
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.integration.channel.QueueChannel
+import org.springframework.integration.config.EnableIntegration
+import org.springframework.integration.dsl.MessageChannels
+import org.springframework.integration.dsl.integrationFlow
+import org.springframework.integration.file.FileReadingMessageSource
+import org.springframework.integration.file.FileReadingMessageSource.WatchEventType.CREATE
+import org.springframework.integration.file.dsl.FileInboundChannelAdapterSpec
+import org.springframework.integration.file.dsl.Files.inboundAdapter
+import org.springframework.integration.file.dsl.Files.toStringTransformer
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.nio.file.Path
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+import kotlin.io.path.moveTo
+import kotlin.io.path.writeText
+
+@ExtendWith(SpringExtension::class)
+class SpringIntegrationFileIntTest {
+
+    @Test
+    fun `file content should be read and transformed and file should be kept`(@Autowired output: QueueChannel) {
+
+        val file = with(tmpPath.resolve("some.txt.write")) {
+            writeText("content")
+            moveTo(tmpPath.resolve("some.txt"))
+        }
+
+        val message = output.receive()
+
+        assertThat(message).matches {
+            it?.payload is String && it.payload == "CONTENT"
+        }
+        assertThat(file).exists()
+    }
+
+    @EnableIntegration
+    @TestConfiguration
+    class Configuration {
+
+        @Bean
+        fun output(): QueueChannel = MessageChannels.queue().get()
+
+        @Bean
+        fun flow() = integrationFlow(
+                filesFromDirectory(tmpPath) {
+                    patternFilter("*.txt")
+                    watchEvents(CREATE)
+                    useWatchService(true)
+                }, {
+                    poller {
+                        it.fixedRate(1, TimeUnit.SECONDS, 1)
+                    }
+                }
+        ) {
+            transform(toStringTransformer())
+            transform<String> { it.uppercase(Locale.getDefault()) }
+            channel(output())
+        }
+
+        private fun filesFromDirectory(directory: Path, configurer: FileInboundChannelAdapterSpec.() -> Unit): FileReadingMessageSource {
+            return inboundAdapter(directory.toFile()).also { configurer(it) }.get()
+        }
+    }
+
+    companion object {
+
+        @TempDir
+        @JvmStatic
+        lateinit var tmpPath: Path
+
+    }
+
+}
