69a958ebf060125a584c7625fbc3ef5bb43ee504
Andreas Gebhardt <agebhar1@googlemail.com>
2022-03-12 20:04:54 +0100

    add snippet for »Spring Boot« w/ custom Clock Provider for Java Bean Validation

---
+++ spring-boot/src/test/kotlin/io/github/agebhar1/snippets/JavaBeanValidationCustomClockProviderTest.kt
@@ -0,0 +1,62 @@
+package io.github.agebhar1.snippets
+
+import java.time.Clock
+import java.time.Instant
+import java.time.LocalDate
+import java.time.Month.MARCH
+import java.time.ZoneOffset.UTC
+import javax.validation.ClockProvider
+import javax.validation.Validator
+import javax.validation.constraints.Future
+import javax.validation.constraints.Past
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junitpioneer.jupiter.DefaultLocale
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
+
+@ExtendWith(SpringExtension::class)
+class JavaBeanValidationCustomClockProviderTest(@Autowired private val validator: Validator) {
+
+  internal data class Data(@get:Past val past: LocalDate, @get:Future val future: LocalDate)
+
+  @Test
+  fun `entity should be validated`() {
+
+    val entity = Data(past = LocalDate.of(2022, MARCH, 11), future = LocalDate.of(2022, MARCH, 13))
+
+    val violations = validator.validate(entity)
+
+    assertThat(violations).isEmpty()
+  }
+
+  @Test
+  @DefaultLocale(language = "en")
+  fun `entity should not be validated`() {
+
+    val entity = Data(past = LocalDate.of(2022, MARCH, 12), future = LocalDate.of(2022, MARCH, 12))
+
+    val violations = validator.validate(entity)
+
+    assertThat(violations)
+        .hasSize(2)
+        .extracting("message")
+        .containsExactlyInAnyOrder("must be a past date", "must be a future date")
+  }
+
+  @TestConfiguration(proxyBeanMethods = false)
+  class Configuration {
+
+    @Bean
+    fun customizedValidator() =
+        object : LocalValidatorFactoryBean() {
+          override fun getClockProvider() = ClockProvider {
+            Clock.fixed(Instant.parse("2022-03-12T19:00Z"), UTC)
+          }
+        }
+  }
+}
