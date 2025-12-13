fd1e872be4b7871be7ff3ae13bfdc3a9167adea4
Andreas Gebhardt <agebhar1@googlemail.com>
2025-12-13 20:04:13 +0100

    [WIP] Quarkus CRUD service

---
+++ quarkus/crud/src/main/java/io/github/agebhar1/snippets/quarkus/health/boundary/ApplicationLiveness.java
@@ -0,0 +1,17 @@
+package io.github.agebhar1.snippets.quarkus.health.boundary;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+@ApplicationScoped
+public class ApplicationLiveness implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("application liveness");
+    }
+
+}
---
+++ quarkus/crud/src/test/java/io/github/agebhar1/snippets/quarkus/fruit/FruitResourceIT.java
@@ -0,0 +1,8 @@
+package io.github.agebhar1.snippets.quarkus.fruit;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class FruitResourceIT extends FruitResourceTest {
+    // Execute the same tests but in packaged mode.
+}
