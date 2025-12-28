982c15b844b4462a91dc23e00a72279d0e945f49
Andreas Gebhardt <agebhar1@googlemail.com>
2025-12-28 10:41:56 +0100

    [WIP] middleware/gateway PoC

---
+++ opentelemetry/retrofit/gateway/main.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"context"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/go-logr/stdr"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.38.0"
+	"go.opentelemetry.io/otel/trace"
+)
+
+// https://eli.thegreenplace.net/2019/unix-domain-sockets-in-go/
+// https://opentelemetry.io/docs/concepts/context-propagation/
+// https://github.com/open-telemetry/opentelemetry-go-contrib/tree/main/examples
+// https://opentelemetry.io/docs/languages/go/
+
+const SockAddr = "/tmp/opentelemetry.sock"
+
+type ClientOpCode uint32
+
+type LookUpKey [8 + 32]byte
+
+const (
+	ClientRead ClientOpCode = iota
+	ClientWrite
+)
+
+type Timespec struct {
+	Seconds int64
+	Nanos   int64
+}
+
+type OpenTelemetryEvent struct {
+	OpCode        ClientOpCode // size 4, offset 0
+	QueueRaw      [8 + 1]byte  // size 9, offset 4
+	_             [3]byte      // size 3
+	Ts            Timespec     // size 16, offset 16
+	Key           [32]byte     // size 32, offset 32
+	DroppedEvents uint64       // size 8, offset 64
+}
+
+func (e OpenTelemetryEvent) unixTs() time.Time {
+	return time.Unix(e.Ts.Seconds, e.Ts.Nanos)
+}
+
+func (e OpenTelemetryEvent) lookUpKey() LookUpKey {
+	var key LookUpKey
+	copy(key[0:], e.QueueRaw[:8])
+	copy(key[8:], e.Key[:])
+	return key
+}
+
+func (e OpenTelemetryEvent) String() string {
+	ts := time.Unix(e.Ts.Seconds, e.Ts.Nanos)
+	return fmt.Sprintf("OpCode: %d, Queue: '%s', Ts: %s, Key: %x, DroppedEvents: %d", e.OpCode, string(e.QueueRaw[:]), ts.Format(time.RFC3339Nano), e.Key, e.DroppedEvents)
+}
+
+type SharedSpanProcessor struct {
+	delegate sdktrace.SpanProcessor
+}
+
+func (p SharedSpanProcessor) RealShutdown(ctx context.Context) error {
+	log.Println("Shutting down SharedSpanProcessor")
+	return p.delegate.Shutdown(ctx)
+}
+
+func (p SharedSpanProcessor) OnStart(parent context.Context, s sdktrace.ReadWriteSpan) {
+	p.delegate.OnStart(parent, s)
+}
+
+func (p SharedSpanProcessor) OnEnd(s sdktrace.ReadOnlySpan) {
+	defer p.delegate.OnEnd(s)
+}
+
+func (p SharedSpanProcessor) Shutdown(ctx context.Context) error {
+	log.Println("NoOp Shutting down SharedSpanProcessor")
+	return nil
+}
+
+func (p SharedSpanProcessor) ForceFlush(ctx context.Context) error {
+	return p.delegate.ForceFlush(ctx)
+}
+
+var kv = make(map[LookUpKey]trace.SpanContext)
+var rwlock = sync.RWMutex{}
+
+func serve(conn net.Conn, sp sdktrace.SpanProcessor) {
+
+	var size uint64 // ssize_t
+	if binary.Read(conn, binary.LittleEndian, &size) != nil {
+		return
+	}
+	buff := make([]byte, size)
+	if _, err := io.ReadFull(conn, buff); err != nil {
+		return
+	}
+	serviceName := string(buff)
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithSpanProcessor(sp),
+		sdktrace.WithResource(resource.NewWithAttributes(semconv.SchemaURL, semconv.ServiceName(serviceName))),
+	)
+	tr := tp.Tracer("")
+
+	defer func() {
+		log.Printf("Client disconnected [%s]", serviceName)
+		_ = conn.Close()
+		_ = tp.Shutdown(context.Background())
+	}()
+
+	log.Printf("Client connected [%s]", serviceName)
+	var event OpenTelemetryEvent
+	var read *OpenTelemetryEvent = nil
+	for {
+		err := binary.Read(conn, binary.LittleEndian, &event)
+		if err != nil {
+			break
+		}
+
+		switch event.OpCode {
+		case ClientRead:
+			read = new(event)
+			log.Printf("[%s] read: {%s}", serviceName, event)
+
+		case ClientWrite:
+			log.Printf("[%s] read: {%s}, write: {%s}", serviceName, read, event)
+
+			write := &event
+			ctx := context.Background()
+			if read == nil {
+				_, span := tr.Start(ctx, fmt.Sprintf("produce %s", string(write.QueueRaw[:])), trace.WithTimestamp(write.unixTs()))
+				span.SetStatus(codes.Ok, "")
+				span.SetAttributes(attribute.String("write", fmt.Sprintf("%x", write.lookUpKey())))
+				span.End(trace.WithTimestamp(write.unixTs()))
+
+				rwlock.Lock()
+				kv[write.lookUpKey()] = span.SpanContext()
+				rwlock.Unlock()
+			} else {
+				rwlock.RLock()
+				if sc, ok := kv[read.lookUpKey()]; ok {
+					ctx = trace.ContextWithRemoteSpanContext(ctx, sc)
+				}
+				rwlock.RUnlock()
+
+				_, span := tr.Start(ctx, fmt.Sprintf("produce %s", string(write.QueueRaw[:])), trace.WithTimestamp(read.unixTs()))
+				span.SetStatus(codes.Ok, "")
+				span.SetAttributes(attribute.String("read", fmt.Sprintf("%x", read.lookUpKey())))
+				span.SetAttributes(attribute.String("write", fmt.Sprintf("%x", write.lookUpKey())))
+				span.End(trace.WithTimestamp(write.unixTs()))
+
+				rwlock.Lock()
+				kv[write.lookUpKey()] = span.SpanContext()
+				rwlock.Unlock()
+			}
+		}
+	}
+}
+
+func main() {
+	stdr.SetVerbosity(5)
+
+	// https://pkg.go.dev/go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp#pkg-overview
+	exp, err := otlptracehttp.New(context.Background(), otlptracehttp.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	sp := SharedSpanProcessor{delegate: sdktrace.NewBatchSpanProcessor(exp)}
+
+	if err := os.RemoveAll(SockAddr); err != nil {
+		log.Fatal(err)
+	}
+
+	l, err := net.Listen("unix", SockAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	wg := sync.WaitGroup{}
+	wg.Go(func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				if ctx.Err() != nil {
+					break
+				}
+				log.Fatalf("accept: %v", err)
+			}
+			wg.Go(func() {
+				<-ctx.Done()
+				// TODO already closed by client
+				_ = conn.Close()
+			})
+			wg.Go(func() { serve(conn, sp) })
+		}
+	})
+
+	<-ctx.Done()
+
+	log.Println("Shutting down")
+	_ = l.Close()
+	_ = sp.RealShutdown(context.Background())
+
+	wg.Wait()
+	if err := os.RemoveAll(SockAddr); err != nil {
+		log.Println(err)
+	}
+}
---
+++ opentelemetry/retrofit/middleware/opentelemetry.c
@@ -0,0 +1,271 @@
+#include <errno.h>
+#include <openssl/sha.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <tgmath.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "opentelemetry.h"
+
+typedef enum { STATE_DISCONNECTED, STATE_READY, STATE_PROCESSING, STATE_EXIT } state_t;
+
+static int opentelemetry_fd = -1;
+
+static struct timespec opentelemetry_gateway_timeout = {.tv_sec = 0, .tv_nsec = 25'000'000};
+
+static pthread_t opentelemetry_gateway_thread;
+static pthread_mutex_t opentelemetry_state_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t opentelemetry_state_cond = PTHREAD_COND_INITIALIZER;
+static pthread_mutex_t opentelemetry_event_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static unsigned char opentelemetry_buffer[1024 * 1024] = {};
+static size_t opentelemetry_buffer_size = 0;
+static state_t opentelemetry_state = STATE_DISCONNECTED;
+static uint64_t opentelemetry_dropped_events = 0;
+static OpenTelemetryEvent opentelemetry_event;
+
+static inline void mutex_incr(pthread_mutex_t *mutex, uint64_t *value) {
+    if (pthread_mutex_lock(mutex) != 0) {
+        perror("pthread_mutex_lock");
+    }
+    (*value)++;
+    if (pthread_mutex_unlock(mutex) != 0) {
+        perror("pthread_mutex_unlock");
+    }
+}
+
+static void *opentelemetry_gateway_thread_fn([[maybe_unused]] void *arg) {
+    while (true) {
+        if (pthread_mutex_lock(&opentelemetry_state_mutex) != 0) {
+            perror("pthread_mutex_lock");
+        }
+
+        while (opentelemetry_state != STATE_PROCESSING && opentelemetry_state != STATE_EXIT) {
+            if (pthread_cond_wait(&opentelemetry_state_cond, &opentelemetry_state_mutex) != 0) {
+                perror("pthread_cond_wait");
+            }
+        }
+
+        if (opentelemetry_state == STATE_EXIT) {
+            break;
+        }
+
+        if (pthread_mutex_unlock(&opentelemetry_state_mutex) != 0) {
+            perror("pthread_mutex_unlock");
+        }
+
+        SHA256(opentelemetry_buffer, opentelemetry_buffer_size, opentelemetry_event.key);
+
+        if (pthread_mutex_lock(&opentelemetry_event_mutex) != 0) {
+            perror("pthread_mutex_lock");
+        }
+        opentelemetry_event.dropped_events = opentelemetry_dropped_events;
+        if (pthread_mutex_unlock(&opentelemetry_event_mutex) != 0) {
+            perror("pthread_mutex_unlock");
+        }
+
+        const ssize_t sent = send(opentelemetry_fd, &opentelemetry_event, sizeof(opentelemetry_event), MSG_NOSIGNAL);
+        if (sent == -1) {
+            perror("send");
+        }
+
+        if (pthread_mutex_lock(&opentelemetry_state_mutex) != 0) {
+            perror("pthread_mutex_lock");
+        }
+
+        opentelemetry_state = STATE_READY;
+
+        if (pthread_cond_signal(&opentelemetry_state_cond) != 0) {
+            perror("pthread_cond_signal");
+        }
+
+        if (pthread_mutex_unlock(&opentelemetry_state_mutex) != 0) {
+            perror("pthread_mutex_unlock");
+        }
+    }
+    if (pthread_mutex_unlock(&opentelemetry_state_mutex) != 0) {
+        perror("pthread_mutex_unlock");
+    }
+    return nullptr;
+}
+
+int opentelemetry_connect() {
+    const char *otel_sdk_disabled = nullptr;
+    if ((otel_sdk_disabled = getenv("OTEL_SDK_DISABLED")) != NULL && strcmp(otel_sdk_disabled, "true") == 0) {
+        return 0;
+    }
+
+    const char *gateway_timeout = getenv("GATEWAY_TIMEOUT");
+    if (gateway_timeout != nullptr) {
+        const double value = atof(gateway_timeout);
+        if (value < 0.0) {
+            fprintf(stderr, "ERROR: GATEWAY_TIMEOUT must be >= 0.0\n");
+        } else {
+            opentelemetry_gateway_timeout.tv_sec = (time_t) value;
+            opentelemetry_gateway_timeout.tv_nsec = (long) ((value - floor(value)) * 1e9);
+        }
+    }
+
+    char *otel_service_name = nullptr;
+    if ((otel_service_name = getenv("OTEL_SERVICE_NAME")) != NULL) {
+        otel_service_name = strdup(otel_service_name);
+    }
+    if (otel_service_name == nullptr || strlen(otel_service_name) == 0) {
+        if (otel_service_name != nullptr) { free(otel_service_name); }
+        return 0;
+    }
+
+    opentelemetry_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (opentelemetry_fd == -1) {
+        free(otel_service_name);
+        return -1;
+    }
+
+    struct sockaddr_un addr = {.sun_family = AF_LOCAL};
+    strcpy(addr.sun_path, "/tmp/opentelemetry.sock");
+
+    if (connect(opentelemetry_fd, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
+        close(opentelemetry_fd);
+        free(otel_service_name);
+        return -1;
+    }
+
+    const ssize_t size = strlen(otel_service_name);
+    if (send(opentelemetry_fd, &size, sizeof(size), MSG_NOSIGNAL) == sizeof(size)) {
+        (void) send(opentelemetry_fd, otel_service_name, size, MSG_NOSIGNAL);
+    }
+
+    free(otel_service_name);
+
+    opentelemetry_state = STATE_DISCONNECTED;
+    if (pthread_create(&opentelemetry_gateway_thread, nullptr, opentelemetry_gateway_thread_fn, nullptr) != 0) {
+        perror("pthread_create");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_mutex_lock(&opentelemetry_state_mutex) != 0) {
+        perror("pthread_mutex_lock");
+    }
+
+    opentelemetry_state = STATE_READY;
+
+    if (pthread_cond_signal(&opentelemetry_state_cond) != 0) {
+        perror("pthread_cond_signal");
+    }
+    if (pthread_mutex_unlock(&opentelemetry_state_mutex) != 0) {
+        perror("pthread_mutex_unlock");
+    }
+
+    return 0;
+}
+
+int opentelemetry_disconnect() {
+    if (pthread_mutex_lock(&opentelemetry_state_mutex) != 0) {
+        perror("pthread_mutex_lock");
+    }
+
+    if (opentelemetry_state != STATE_DISCONNECTED) {
+        while (opentelemetry_state != STATE_READY) {
+            if (pthread_cond_wait(&opentelemetry_state_cond, &opentelemetry_state_mutex) != 0) {
+                perror("pthread_cond_wait");
+            }
+        }
+
+        opentelemetry_state = STATE_EXIT;
+
+        if (pthread_cond_signal(&opentelemetry_state_cond) != 0) {
+            perror("pthread_cond_signal");
+        }
+        if (pthread_mutex_unlock(&opentelemetry_state_mutex) != 0) {
+            perror("pthread_mutex_unlock");
+        }
+        if (pthread_join(opentelemetry_gateway_thread, nullptr) != 0) {
+            perror("pthread_join");
+        }
+    } else {
+        if (pthread_mutex_unlock(&opentelemetry_state_mutex) != 0) {
+            perror("pthread_mutex_unlock");
+        }
+    }
+
+    opentelemetry_state = STATE_DISCONNECTED;
+
+    if (opentelemetry_fd == -1) {
+        return 0;
+    }
+
+    const int r = close(opentelemetry_fd);
+    opentelemetry_fd = -1;
+
+    return r;
+}
+
+static int opentelemetry_execute(const ClientOpCode opcode, const char *queue, const unsigned char *buffer,
+                                 const size_t size) {
+    if (opentelemetry_fd == -1) {
+        return -1;
+    }
+
+    struct timespec abs_timeout;
+    clock_gettime(CLOCK_REALTIME, &abs_timeout);
+
+    abs_timeout.tv_sec += opentelemetry_gateway_timeout.tv_sec;
+    abs_timeout.tv_nsec += opentelemetry_gateway_timeout.tv_nsec;
+    abs_timeout.tv_sec += abs_timeout.tv_nsec / 1'000'000'000;
+    abs_timeout.tv_nsec = abs_timeout.tv_nsec % 1'000'000'000;
+
+    const int rv_mutex = pthread_mutex_timedlock(&opentelemetry_state_mutex, &abs_timeout);
+    if (rv_mutex != 0) {
+        perror("pthread_mutex_timedlock");
+        return -1;
+    }
+    if (rv_mutex == ETIMEDOUT) {
+        mutex_incr(&opentelemetry_event_mutex, &opentelemetry_dropped_events);
+        return ETIMEDOUT;
+    }
+
+    while (opentelemetry_state != STATE_READY) {
+        const int rv_cond = pthread_cond_timedwait(&opentelemetry_state_cond, &opentelemetry_state_mutex, &abs_timeout);
+        if (rv_cond == ETIMEDOUT) {
+            mutex_incr(&opentelemetry_event_mutex, &opentelemetry_dropped_events);
+            if (pthread_mutex_unlock(&opentelemetry_state_mutex) != 0) {
+                perror("pthread_mutex_unlock");
+            }
+            return ETIMEDOUT;
+        }
+    }
+
+    opentelemetry_event.opcode = opcode;
+
+    strncpy(opentelemetry_event.queue, queue, sizeof(opentelemetry_event.queue) - 1);
+    opentelemetry_event.queue[sizeof(opentelemetry_event.queue) - 1] = '\0';
+
+    clock_gettime(CLOCK_REALTIME, &opentelemetry_event.ts);
+
+    memcpy(opentelemetry_buffer, buffer, size);
+    opentelemetry_buffer_size = size;
+    opentelemetry_state = STATE_PROCESSING;
+
+    if (pthread_cond_signal(&opentelemetry_state_cond) != 0) {
+        perror("pthread_cond_signal");
+    }
+
+    if (pthread_mutex_unlock(&opentelemetry_state_mutex) != 0) {
+        perror("pthread_mutex_unlock");
+    }
+
+    return 0;
+}
+
+int opentelemetry_read(const char *queue, const unsigned char *buffer, const size_t size) {
+    return opentelemetry_execute(CLIENT_READ, queue, buffer, size);
+}
+
+int opentelemetry_write(const char *queue, const unsigned char *buffer, const size_t size) {
+    return opentelemetry_execute(CLIENT_WRITE, queue, buffer, size);
+}
