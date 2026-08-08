d08a2d8c182af6bccd4a3289d703f948d76363e4
Andreas Gebhardt <agebhar1@googlemail.com>
2026-08-08 11:00:12 +0200

    [WIP] Vite + Vue / Typescript + Vitest + ESLint

---
+++ vue/nuxt-ui/README.md
@@ -0,0 +1,7 @@
+## Links
+
+* [Vite](https://vite.dev/)
+* [Vue.js](https://vuejs.org/)
+* [Vitest](https://vitest.dev/)
+* [ESLint](https://eslint.org/)
+  * [ESLint Stylistic](https://eslint.style)
---
+++ vue/nuxt-ui/vite.config.ts
@@ -0,0 +1,24 @@
+import { URL, fileURLToPath } from 'node:url'
+
+import { configDefaults } from 'vitest/config'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/**'],
+    root: fileURLToPath(new URL('./', import.meta.url)),
+  },
+})
